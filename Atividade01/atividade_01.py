487c3be7f7051852cfe8f28ead838ef38935c5bf
Matheus <matheus.rj25@hotmail.com>
2023-08-26 12:31:41 -0300

    update atividade 01

--- Atividade01/atividade_01.py
+++ Atividade01/atividade_01.py
@@ -7,9 +7,9 @@ lista_posicao_y = []
 tempo = 0.0
 dt = 0.1
 y_0 = 0.00
-v_0 = 0.00
+v_0 = 18.00
 g = 9.8
-angulo = 80.0
+angulo = 80 
 
 while tempo<3.7:
     v_0y = v_0*math.sin(math.radians(angulo))
@@ -23,5 +23,5 @@ print(lista_posicao_y)
 plt.xlabel('$x$($m$)')
 plt.ylabel('$y$(m)')
 plt.plot(lista_posicao_x,lista_posicao_y)
-plt.show()cd
+plt.show()
 plt.title('Trajetória do projétil')
